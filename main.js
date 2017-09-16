6bff28c4d9500cbec9ff874c1eb07f93f845cee5
Jacky Chen <jacky.c@17.media>
2017-09-16 20:32:17 +0800

    Space reset view

--- main.js
+++ main.js
@@ -1,64 +1,73 @@
-var canvas = document.getElementById('Canvas');
-var context = canvas.getContext('2d');
-var width = 480;
-var height = 360;
-context.beginPath();
-context.moveTo(40,70);
-context.lineTo(400,200);
-context.stroke();
-//
 function view () {
+	var map = document.getElementById('map');
+	var ctx = map.getContext('2d');
+
+	// clear canvas for redrawing
+	ctx.clearRect(0, 0, map.width, map.height);
+
+	// map background
+	ctx.fillStyle = game.mapBackground;
+	ctx.fillRect(0, 0, map.width, map.height);
+
+	// map border
+	ctx.lineWidth = game.borderSize;
+	ctx.strokeRect(0, 0, map.width, map.height);
+	ctx.strokeStyle = game.mapBackground;
+
+	// map grill
+	ctx.beginPath();
+	for(i = 1; i<game.mapSize; i++) {
+		ctx.moveTo(0, map.height/game.mapSize*i);
+		ctx.lineTo(map.width, map.height/game.mapSize*i);
+		ctx.moveTo(map.width/game.mapSize*i, 0);
+		ctx.lineTo(map.width/game.mapSize*i, map.height);
+		ctx.strokeStyle = "rgb(0,0,0)";
+		ctx.lineWidth=game.lineSize;
+	}
+	ctx.closePath();
+	ctx.stroke();
+
+	// map erase line
+	ctx.beginPath();
+	ctx.moveTo(map.width/game.mapSize*1 + 1 , map.height/game.mapSize*2);
+	ctx.lineTo(map.width/game.mapSize*2 - 1 , map.height/game.mapSize*2);	
+	ctx.strokeStyle = game.mapBackground;
+	ctx.lineWidth = game.lineSize + 1;
+	ctx.stroke();
 }
 
-function model () {
-}
+
+// function model () {
+// }
+//
 
 function GameEngine (size) {
-	this.size = size
-	this.startTime = null
+	this.mapSize = size
+	this.mapBackground = "rgb(255,240,240)";
+	this.borderSize = 3
+	this.lineSize = 1
+	this.startTime = $.now()
 	this.mapTree = null
 }
 
-var game = new GameEngine(3);
-game.startTime = $.now()
 GameEngine.prototype.starGame = function() {
 	// construct puzzle
 
 	// set model, show puzzle
 
 	// count down
-	// setInterval(function(){
-	//     // do
-	// }, 30)
-}
 
-$(function() {
-
-	var cursor = '<div id="cursor"></div>';
-	$("#map").append(cursor)
-
+	// update view
 	$(document).keydown(function(e) {
-		var position = $("#cursor").position();
-		alert(e.KeyCode);
-		// alert(position);
+		console.log(e.keyCode);
 
-		switch (e.KeyCode)
+		switch (e.keyCode)
 		{
-			case 37:	// left
-				$("#cursor").css('left', position.left - 20 + 'px');
-				break;
-
-			case 38:	// up
-				$("#cursor").css('top', position.top - 20 + 'px');
-				break;
-
-			case 39:	// right
-				$("#cursor").css('left', position.left + 20 + 'px');
-				break;
-
-			case 40:	// down
-				$("#cursor").css('top', position.top + 20 + 'px');
-				break;
+			case 32:	// space	
+				view();
 		}
 	});
-});
+}
+
+var game = new GameEngine(4);
+game.starGame();
