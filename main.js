8024d406063774c28d713af4abbcd5ea48191a8e
Jacky Chen <jacky.c@17.media>
2017-09-17 16:50:13 +0800

    Draw random startPoint and endPoint

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ function View () {
 	this.ctx = map.getContext('2d');
 	this.ww = map.width;
 	this.hh = map.height;
-	this.w = this.ww / game.mapSize;
-	this.h = this.hh / game.mapSize;
+	this.w = this.ww / game.puzzleWidth;
+	this.h = this.hh / game.puzzleHeight;
 }
 
 View.prototype.drawPuzzle = function() {
@@ -12,6 +12,7 @@ View.prototype.drawPuzzle = function() {
 	this.drawBackground();
 	this.drawGrid();
 	this.eraseLine();
+	this.drawStartEnd();
 }
 
 View.prototype.clearCanvas = function() {
@@ -19,7 +20,7 @@ View.prototype.clearCanvas = function() {
 }
 
 View.prototype.drawBackground = function() {
-	this.ctx.fillStyle = game.mapBackground;
+	this.ctx.fillStyle = game.backgroundColor;
 	this.ctx.fillRect(0, 0, this.ww, this.hh);
 	this.ctx.lineWidth = game.borderLineWidth;
 	this.ctx.strokeStyle = "rgb(0,0,0)";
@@ -33,7 +34,7 @@ View.prototype.drawBlackBackground = function() {
 
 View.prototype.drawGrid = function() {
 	this.ctx.beginPath();
-	for(i = 1; i<game.squareNum - 1; i++) {
+	for(i = 1; i<game.puzzleWidth * game.puzzleHeight - 1; i++) {
 		this.ctx.moveTo(0, this.h * i);
 		this.ctx.lineTo(this.ww, this.h * i);
 		this.ctx.moveTo(this.w * i, 0);
@@ -46,72 +47,87 @@ View.prototype.drawGrid = function() {
 
 View.prototype.eraseLine = function() {
 	this.ctx.beginPath();
-	for(i = 0; i<game.squareNum; i++) {
-		x = i%4;
-		y = Math.floor(i/4);
+	for(i = 0; i<game.puzzleWidth * game.puzzleHeight; i++) {
+		x = i % game.puzzleWidth;
+		y = Math.floor(i / game.puzzleWidth);
 
-		if((game.puzzle[i] & 1) == 1) {
-				this.ctx.moveTo(this.w * x, this.h * y + game.gridLineWidth/2);
-				this.ctx.lineTo(this.w * x, this.h * (y+1) - game.gridLineWidth/2);
+		if((game.puzzle.map[i] & 1) == 1) {
+			this.ctx.moveTo(this.w * x, this.h * y + game.gridLineWidth/2);
+			this.ctx.lineTo(this.w * x, this.h * (y+1) - game.gridLineWidth/2);
 		}
-		if((game.puzzle[i] & 2) == 2) {
-				this.ctx.moveTo(this.w * x + game.gridLineWidth/2, this.h * y);
-				this.ctx.lineTo(this.w * (x+1) - game.gridLineWidth/2, this.h * y);
+		if((game.puzzle.map[i] & 2) == 2) {
+			this.ctx.moveTo(this.w * x + game.gridLineWidth/2, this.h * y);
+			this.ctx.lineTo(this.w * (x+1) - game.gridLineWidth/2, this.h * y);
 		}
-		if((game.puzzle[i] & 4) == 4) {
-				this.ctx.moveTo(this.w * (x + 1), this.h * y + game.gridLineWidth/2);
-				this.ctx.lineTo(this.w * (x + 1), this.h * (y+1) - game.gridLineWidth/2);
+		if((game.puzzle.map[i] & 4) == 4) {
+			this.ctx.moveTo(this.w * (x + 1), this.h * y + game.gridLineWidth/2);
+			this.ctx.lineTo(this.w * (x + 1), this.h * (y+1) - game.gridLineWidth/2);
 		}
-		if((game.puzzle[i] & 8) == 8) {
-				this.ctx.moveTo(this.w * x + game.gridLineWidth/2, this.h * (y+1));
-				this.ctx.lineTo(this.w * (x+1) - game.gridLineWidth/2, this.h * (y+1));
+		if((game.puzzle.map[i] & 8) == 8) {
+			this.ctx.moveTo(this.w * x + game.gridLineWidth/2, this.h * (y+1));
+			this.ctx.lineTo(this.w * (x+1) - game.gridLineWidth/2, this.h * (y+1));
 		}
 	}
-	this.ctx.strokeStyle = game.mapBackground;
+	this.ctx.strokeStyle = game.backgroundColor;
 	this.ctx.lineWidth = game.gridLineWidth + 1;
 	this.ctx.stroke();
+}
+
+View.prototype.drawStartEnd = function() {
+	// draw startPoint
+	startX = game.puzzle.startPoint % game.puzzleWidth
+	startY = Math.floor(game.puzzle.startPoint / game.puzzleWidth)
+	this.ctx.fillStyle = "blue";
+	this.ctx.fillRect(startX * this.w, startY * this.h, this.w, this.h);
+
+	// draw endPoint
+	endX = game.puzzle.endPoint % game.puzzleWidth
+	endY = Math.floor(game.puzzle.endPoint / game.puzzleWidth)
+	this.ctx.fillStyle = "red";
+	this.ctx.fillRect(endX * this.w, endY * this.h, this.w, this.h);
 }
 
 // function model () {
 // }
 //
 
-function GameEngine (size) {
-	this.mapSize = size
-	this.mapBackground = "rgb(255,240,240)";
+function GameEngine (width, height) {
+	this.backgroundColor = "rgb(255,240,240)";
 	this.borderLineWidth = 3
 	this.gridLineWidth = 2
 	this.startTime = $.now()
-	this.squareNum = Math.pow(size, 2) 
-	this.puzzle = Array[Math.pow(size, 2)]
-	this.startPoint
-	this.endPoint
-	this.constructPuzzleMethod = deadPuzzle // or randomPuzzle
+
+	this.puzzleWidth = width
+	this.puzzleHeight = height
+	this.puzzle = {map: Array(width * height), startPoint: 0, endPoint: width * height -1}
+	this.constructPuzzleMethod = randomPuzzle
 	this.state = 0;
 	this.FPS = 25;
 
 	this.engine = setInterval(() => {
 		console.log(this.state)
 		if(this.state == 0) {
+			this.constructNewPuzzle()
+			console.log(this.puzzle)
+			this.state = 1
+		}
+
+		if(this.state == 1) {
 			view.drawBackground();
 		}
-		else if(this.state == 1) {
-			view.drawPuzzle();
-
-			if($.now() - this.startTime > 1000) this.state = 2;
-		}
 		else if(this.state == 2) {
-			view.drawBlackBackground();
+			if($.now() - this.startTime < 1000) view.drawPuzzle()
+			else view.drawBlackBackground()
 		}
-	}, 1000 / this.FPS);
+	}, 1000 / this.FPS)
 }
 
 GameEngine.prototype.startGame = function() {
 
-	var self = this;
-	
+	var self = this
+
 	// coonstruct puzzle
-	this.constructPuzzle();
+	this.constructNewPuzzle()
 
 	// set model, show puzzle
 
@@ -119,47 +135,66 @@ GameEngine.prototype.startGame = function() {
 
 	// update View
 	$(document).keydown(function(e) {
-		console.log(e.keyCode);
+		// console.log(e.keyCode)
 
 		switch (e.keyCode) {
 			case 32:	// space	
 				switch (self.state) {
-					case 0:
-						self.state = 1;
-						self.startTime = $.now();
-						break;
+					case 1:
+						self.state = 2
+						self.startTime = $.now()
+						break
 					case 2:
-						self.state = 0;
-						break;
+						self.state = 0
+						break
 				}
 		}
-	});
+	})
 }
 
-GameEngine.prototype.constructPuzzle = function() {
+GameEngine.prototype.constructNewPuzzle = function() {
 	// do something
-	this.startPoint = Math.floor(Math.random() * this.squareNum);
-	this.endPoint = Math.floor(Math.random() * (this.squareNum-1));
-	if(this.endPoint >= this.startPoint ) this.endPoint++; 
-	
+
 	// construct puzzle
-	this.puzzle = this.constructPuzzleMethod(this.mapSize, this.startPoint, this.endPoint);
+	this.puzzle = this.constructPuzzleMethod(this.puzzleWidth, this.puzzleHeight, this.startPoint, this.endPoint);
 
 	// check something
 }
 
-deadPuzzle = function(size, startPoint, endPoint) {
-	return [0,1,5,8,8,1,2,8,8,2,8,1,0,2,5,0]
+deadPuzzle = function(width, height) {
+	return { 
+		map: [
+			0,1,5,8,0,0,
+			8,1,2,8,0,0,
+			8,2,8,1,0,0,
+			0,2,5,0,0,0,
+			0,0,0,0,0,0],
+		startPoint: 0,
+		endPoint: 15,
+	}
 }
 
-randomPuzzle = function(size, startPoint, endPoint) {
-	return [0,1,5,8,8,1,2,8,8,2,8,1,0,2,5,0]
+randomPuzzle = function(width, height, startPoint, endPoint) {
+	startPoint = Math.floor(Math.random() * width * height);
+	endPoint = Math.floor(Math.random() * (width * height - 1));
+	if(endPoint >= startPoint ) endPoint++; 
+
+	return { 
+		map: [ 
+			0,1,5,8,0,0,
+			8,1,2,8,0,0,
+			8,2,8,1,0,0,
+			0,2,5,0,0,0,
+			0,0,0,0,0,0],
+		startPoint: startPoint,
+		endPoint: endPoint,
+	}
 }
 
 // GameEngine.prototype.stopGame = function() {
 //     clearInterval(this.engine);
 // }
 
-var game = new GameEngine(4);
+var game = new GameEngine(6, 5);
 var view = new View();
 game.startGame();
